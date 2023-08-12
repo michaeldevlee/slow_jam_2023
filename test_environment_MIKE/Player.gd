16c45f86880c995f3af132afab6e1b3e5d6edfe9
mikelee53 <mikeleevt@gmail.com>
2023-08-11 21:29:41 -0700

    added basic top down player movement

---
+++ test_environment_MIKE/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var speed = 50
+var velocity = Vector2()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	elif Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	elif Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	elif Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
+
