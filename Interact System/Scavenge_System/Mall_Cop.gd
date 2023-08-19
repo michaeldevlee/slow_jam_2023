532681e7dac44feb60d7de4ffef5231ec0db07c8
mikelee53 <mikeleevt@gmail.com>
2023-08-18 20:10:55 -0700

    add mall cop sensing area and chasing area

--- Interact System/Scavenge_System/Mall_Cop.gd
+++ Interact System/Scavenge_System/Mall_Cop.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 export var speed = 30
 onready var cast = get_node("RayCast2D")
 onready var tween = get_node("Tween")
+onready var sensingArea = get_node("SensingArea")
+onready var chaseArea = get_node("ChaseArea")
 
 var velocity = Vector2()
 export (String, "PATROL", "ALERT", "CHASE") var curr_state = "PATROL"
@@ -10,8 +12,9 @@ export (String, "UP", "DOWN", "LEFT", "RIGHT") var move_direction = "DOWN"
 
 var patrolFrom : Vector2
 export var patrolTo : Vector2
+var player : KinematicBody2D
 
-enum travel_states {TRAVELING_TO, TRAVELING_BACK}
+enum travel_states {TRAVELING_TO, TRAVELING_BACK, }
 var travel_status
 
 
@@ -19,6 +22,8 @@ func _ready():
 	patrolFrom = position
 	patrolTo = patrolFrom + patrolTo
 	travel_status = travel_states.TRAVELING_TO
+	sensingArea.connect("body_entered", self, "chase_if_confirmed")
+	chaseArea.connect("body_exited", self, "go_back_to_patrol")
 
 			
 func switchState(state, delta):
@@ -38,10 +43,17 @@ func switchState(state, delta):
 		"ALERT":
 			pass
 		"CHASE":
-			pass
+			position = position.move_toward(player.position, delta*speed)
 
+func chase_if_confirmed(body):
+	if body.name == "Player":
+		curr_state = "CHASE"
+		player = body
 
-
+func go_back_to_patrol(body):
+	if body.name == "Player":
+		curr_state = "PATROL"
+		player = null
 
 func _physics_process(delta):
 	switchState(curr_state, delta)
