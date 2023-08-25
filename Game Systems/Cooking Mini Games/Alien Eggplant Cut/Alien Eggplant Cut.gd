0c6b60369094c3bd2fee5fe12ff20660022a525a
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2023-08-24 19:45:09 -0700

    working build of alien eggplant mini game

--- Game Systems/Cooking Mini Games/Alien Eggplant Cut/Alien Eggplant Cut.gd
+++ Game Systems/Cooking Mini Games/Alien Eggplant Cut/Alien Eggplant Cut.gd
@@ -1,14 +1,28 @@
 extends MiniGame
 
-onready var circle = get_node("Area2D/CollisionShape2D")
-onready var circle_sprite = get_node("Area2D/Sprite3")
+onready var circle = get_node("SelectionArea/CollisionShape2D")
+onready var circle_sprite = get_node("SelectionArea/Sprite3")
+onready var eggplant_root = get_node("Eggplants")
+onready var eggplant_timer = get_node("Eggplant_Timer")
 var drawing : bool = false
 var original_mos_pos = Vector2()
 var original_collision_radius
+var eggplant = preload("res://Game Systems/Cooking Mini Games/Alien Eggplant Cut/Alien Eggplant.tscn")
+var rng = RandomNumberGenerator.new()
+
 
 func _ready():
+	rng.randomize()
 	original_collision_radius = circle.shape.radius
+	eggplant_timer.connect("timeout", self, "spawn_eggplant")
 
+func spawn_eggplant():
+	var eggplant_instance = eggplant.instance()
+	var rand_x = rng.randi_range(30, 200)
+	var rand_y = rng.randi_range(30, 150)
+	
+	eggplant_instance.global_position = Vector2(rand_x, rand_y)
+	eggplant_root.add_child(eggplant_instance)
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("mouse_interact"):
 		drawing = true
@@ -25,11 +39,12 @@ func _unhandled_input(event):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
 		
+		
 	
 	if event is InputEventMouseMotion:
-		if drawing:
+		if drawing and circle.shape.radius < 25:
 			print(event.speed)
-			circle.shape.radius += 2
+			circle.shape.radius += 4
 			
 			var target_scale = circle.shape.radius / original_collision_radius
 			
---
+++ Game Systems/Cooking Mini Games/Alien Eggplant Cut/Alien Eggplant.gd
@@ -0,0 +1,25 @@
+extends Sprite
+
+var selected : bool = false
+var selection_points 
+var count = 0
+
+func _ready():
+	selection_points = get_children().size()
+	
+	for area in get_children():
+		area.connect("area_entered", self, "toggle_select", [true])
+		area.connect("area_exited", self, "toggle_select", [false])
+
+func toggle_select(area, status):
+	if area.name == "SelectionArea":
+		selected = status
+		print('selected is '  + str(status))
+	
+		if selected:
+			count += 1
+		else:
+			count -= 1
+		
+		if count == selection_points:
+			queue_free()
